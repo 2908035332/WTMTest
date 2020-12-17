eab03cb6281cdda8643eb7a79a1e3e4f87a2b93a
PHR <2908035332@qq.com>
2020-12-17 16:33:05 +0800

    Revert "123"
    This reverts commit 87d7a6eac6de72f257f5e82c77345ea45954bbb0.

---
+++ MyTest.ViewModel/MyUserVMs/MyUserSearcher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using WalkingTec.Mvvm.Core;
+using WalkingTec.Mvvm.Core.Extensions;
+using MyTest.Model;
+
+
+namespace MyTest.ViewModel.MyUserVMs
+{
+    public partial class MyUserSearcher : BaseSearcher
+    {
+        [Display(Name = "Account")]
+        public String ITCode { get; set; }
+        [Display(Name = "Name")]
+        public String Name { get; set; }
+        [Display(Name = "Sex")]
+        public SexEnum? Sex { get; set; }
+        [Display(Name = "CellPhone")]
+        public String CellPhone { get; set; }
+        public List<ComboSelectListItem> AllUserRoless { get; set; }
+        [Display(Name = "Role")]
+        public List<Guid> SelectedUserRolesIDs { get; set; }
+
+        protected override void InitVM()
+        {
+            AllUserRoless = DC.Set<FrameworkRole>().GetSelectListItems(LoginUserInfo?.DataPrivileges, null, y => y.RoleName);
+        }
+
+    }
+}
